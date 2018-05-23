4bd760eea850aa6e498a36abeb3918716dcbcc17
Giovanni Lopez Celaya <giovanni.lopez3523@gmail.com>
2018-05-23 09:28:12 -0700

    Tarea 6 Lista

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,33 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-
 naive_bayes.py
 ---------------
-
 Archivo general para realizar las pruebas del uso del método de bayes inocente.
-
 Se prueba en un conjunto de prueba de dimensión fija sobre cadenas de ADN.
 Para mayor información sobre la base de datos, se puede consultar en
-
 http://archive.ics.uci.edu/ml/datasets/Molecular+Biology+(Splice-junction+Gene+Sequences)
-
 La base ya se trato, y se convirtieron los valores de genes a numeros enteros
 con el fin de poder utilizar la base para varios métodos de clasificación (como
 redes neuronales).
-
 La base de datos ya se repartió en una base de entrenamiento (dna.data) y otra
 base de prueba(dna.test). Con el fin de probar la robustez de los algoritmos de
 aprendizaje ante ruido, se agregó a la base otros atributos extra con valores
 asignados al azar (como si tuvieramos atributos extra con información no
 significativa). Estos datos se encuentran en dna_noise.data y dna_noise.test
 respectivamente.
-
-Para estar seguro que el algoritmo funciona, tanto sin ruido como con
-ruido el error de clasificación con los datos originales debe estar
-por debajo del 5%, mientras que el error en el conjunto de prueba debe
-de andar un poco por arriba del 5% pero claramente menor al 7%
-
+Para estar seguro que el algoritmo funciona, tanto sin ruido como con ruido el
+error de clasificación con los datos originales debe estar por debajo del 5%,
+mientras que el error en el conjunto de prueba debe de andar un poco por arriba
+del 5% pero claramente menor al 7%
 """
 
 import nb
@@ -36,14 +28,11 @@ import nb
 def carga_archivo(archivo):
     """
     Cargar el archivo de datos a clasificar, devuelve
-
     - datos = [dato1, dato2, ..., datoE], la lista de E datos a clasificar
       donde dato1 = [dato1(1), ..., dato1(n)] son los n valores de los
       atributos de dato1.
-
     - clases = [clase1, clase2, ..., claseE] la clase a la que pertenece
       cada dato
-
     """
     datos, clases = [], []
 
@@ -58,7 +47,6 @@ def carga_archivo(archivo):
 def error_clasif(c1, c2):
     """
     Encuentra el porcentaje de valores diferentes entre la lista c1 y la c2
-
     """
     acc = len([1 for i in range(len(c1)) if c1[i] != c2[i]])
     return 1.0 * acc / len(c1)
@@ -66,8 +54,8 @@ def error_clasif(c1, c2):
 
 def main():
 
-    print("\nPrueba con la base de datos de DNA sin ruido")
-    print("----------------------------------------------")
+    print "\nPrueba con la base de datos de DNA sin ruido"
+    print "----------------------------------------------"
 
     datos, clases = carga_archivo("dna.data")
     clasificador = nb.NaiveBayes(range(len(datos[0])))
@@ -75,17 +63,17 @@ def main():
     clasificador.aprende(datos, clases)
     clases_estimadas = clasificador.reconoce(datos)
     error = error_clasif(clases, clases_estimadas)
-    print("Error de estimación en los mismos datos: " +
-          str(error*100)+" %")
+    print (u"Error de estimación en los mismos datos: " +
+           str(error*100)+" %")
 
     d_test, c_test = carga_archivo("dna.test")
     c_e_test = clasificador.reconoce(d_test)
     e_test = error_clasif(c_test, c_e_test)
-    print("Error de estimación en los datos de prueba: " +
-          str(e_test*100)+" %\n")
+    print (u"Error de estimación en los datos de prueba: " +
+           str(e_test*100)+" %\n")
 
-    print("\nPrueba con la base de datos de DNA con ruido")
-    print("----------------------------------------------")
+    print "\nPrueba con la base de datos de DNA con ruido"
+    print "----------------------------------------------"
 
     datos, clases = carga_archivo("dna_noise.data")
     clasificador_ruido = nb.NaiveBayes(range(len(datos[0])))
@@ -93,12 +81,12 @@ def main():
     clasificador_ruido.aprende(datos, clases)
     clases_estimadas = clasificador_ruido.reconoce(datos)
     error = error_clasif(clases, clases_estimadas)
-    print("Error de estimación en los mismos datos: "+str(error*100)+"%")
+    print u"Error de estimación en los mismos datos: "+str(error*100)+"%"
 
     d_test, c_test = carga_archivo("dna_noise.test")
     c_e_test = clasificador_ruido.reconoce(d_test)
     e_test = error_clasif(c_test, c_e_test)
-    print("Error de estimación en los datos de prueba: "+str(e_test*100)+"%\n")
+    print u"Error de estimación en los datos de prueba: "+str(e_test*100)+"%\n"
 
 
 if __name__ == "__main__":
--- nb.py
+++ nb.py
@@ -3,239 +3,165 @@
 """
 nb.py
 ------------
-
 Clase genérica para realizar el método de clasificación de naive bayes.
-
 """
 
 __author__ = 'juliowaissman'
 
-from math import log
 
-
-class NaiveBayes:
+class NaiveBayes(object):
     """
-    Clase genérica del clasificador naive bayes, para entradas con
-    dominio discreto y finito
-
+    Clase genérica del clasificador naive bayes, para entradas con dominio discreto y finito
     """
 
-    def __init__(self, clases=None, variables=None, valores=None):
-        """Inicializa el algoritmo de NB
-
-        @param clases: es un conjuto (o lista) [clase1, ..., clasek],
-                       con el nombre de las k clases que nos
-                       interesan.
-
-        @param variables: Lista con los nombres de las variabes
-                          que se usan, en el orden en que vienen
-                          organizadas dentro de los datos con los que
-                          se alimenta al método de aprendizaje.
-
-        @param valores: Diccionario donde valores[var] es una lista
-                        o un conjunto con los valores que puede tomar
-                        la variable en cuestión.
-
+    def __init__(self, variables=(), vals=None, cls_nombre=None):
         """
-        self.clases = clases
-        self.var_nom = variables
-        self.vals = valores
-        if (clases is not None and
-            variables is not None and
-            valores is not None):
-            self.inicializa_cuentas()
+        Inicializa el algoritmo con vars el nombre de las variables de los atributos (no la clase),
+        y valos un diccionario donde
+            vals[var] = set([val1, ..., valm])
+        son los valores que puede tomar la variable var. Si vals es None, entonces vals se construye
+        con los valores que se tienen en el conjunto de aprendizaje.
+        clases es un conjuto (o lista) [clase1, ..., clasek], con el nombre de las k clases que nos
+        interesan. Si clases == None o vals == None entonces no se inicializa el diccionario de
+        frecuencias, y se espera a que inicie el aparendizaje para inicializar las cuentas. Para
+        mas información sobre como manejaremos las frecuencias, revisar la documentación del método
+        genera_cuentas.
+        """
+        self.vars = variables
+        self.vals = vals
+        self.cls_nombre = cls_nombre
+        self.num_datos = 0
+        if vals is not None and cls_nombre is not None:
+            self.genera_cuentas()
         else:
-            self.frec = None
-            self.log_probs = None
+            self.cuentas = None
 
-    def inicializa_cuentas(self):
+    def genera_cuentas(self):
         """
-        Para poder hacer un aprendizaje incremental, esto es, poder
-        agregar nuevos ejemplos de aprendizaje en linea, en lugar de
-        guardar las CPTs, vamos a utilizar un diccionario con
-        frecuencias.
-
-        self.frecuencias['clases'] es un diccionario, donde en cada
-        nombre de clase guarda la frecuencia encontrada (esto permite
-        inclusive agregar nuevas clases en linea). Así, por cada valor
-        en la lista self.cls_nombre habra una entrada del diccionario
-        self.frecuencias['clases].
-
+        Para poder hacer un aprendizaje incremental, esto es, poder agregar nuevos ejemplos de aprendizaje en
+        linea, en lugar de guardar las CPTs, vamos a utilizar un diccionario con frecuencias.
+        self.frecuencias['clases'] es un diccionario, donde en cada nombre de clase guarda la frecuencia encontrada
+        (esto permite inclusive agregar nuevas clases en linea). Así, por cada valor en la lista self.cls_nombre
+        habra una entrada del diccionario self.frecuencias['clases].
         Por cada variable habrá otros dicionarios, de manera que
-        self.frecuencias[var][clase][val] es el numero de ocurrencias
-        del valor val, de la variable var, cuando los datos están
-        asociados a la clase clase.
-
-        De la misma manray para facilitar el reconocimiento se utiliza
-        un diccionario self.log_probs
-
+            self.frecuencias[var][clase][val]
+        es el numero de ocurrencias del valor val, de la variable var, cuando los datos están asociados a la
+        clase clase.
         """
-        self.frec = {var: {clase: {val: 0 for val in self.vals[var]}
-                           for clase in self.clases}
-                     for var in self.var_nom}
-        self.frec['clases'] = {clase: 0 for clase in self.clases}
-
-        self.log_probs = {var: {clase: {val: 0 for val in self.vals[var]}
-                                for clase in self.clases}
-                          for var in self.var_nom}
-        self.log_probs['clases'] = {clase: 0 for clase in self.clases}
+        self.cuentas = {var: {clase: {val: 0 for val in self.vals[var]}
+                              for clase in self.cls_nombre}
+                        for var in self.vars}
+        self.cuentas['clases'] = {clase: 0 for clase in self.cls_nombre}
 
     def aprende(self, datos, clases):
         """
         Aprende los valores de la CPT, es el trabajo a realizar
-
-        @param datos: Lista [dato_1, ..., dato_N],
-                      donde dato_i es a su ves una lista
-                      tal que dato_i = [d_i1, ..., d_in]
-                      es el vector quel i-ésimo dato.
-
-        @param clases: Lista [clase_1, ..., clase_N] con
-                       las clases correspondientes a cada dato
-                       dato_i de la estructura anterior.
-
         """
-
-        # Inicializa a cero todas las cuentas si no hay un valr previo
-        # (en un futuro sería importante verificar si no hay algun valor nuevo
-        # que no se hubiera agregado antes).
-        inicializar = False
-        if self.var_nom is None:
-            self.var_nom = [str(i) for i in range(len(datos[0]))]
-            inicializar = True
         if self.vals is None:
-            self.vals = {var: set([datos[j][i] for j in range(len(datos))])
-                         for (i, var) in enumerate(self.var_nom)}
-            inicializar = True
-        if self.clases is None:
-            self.clases = set(clases)
-            inicializar = True
-        if inicializar:
-            self.inicializa_cuentas()
+            self.vals = {self.vars[i]: set([datos[j][i] for j in range(len(datos))]) for i in range(len(self.vars))}
+        if self.cls_nombre is None:
+            self.cls_nombre = set(clases)
+        if self.cuentas is None:
+            self.genera_cuentas()
 
-        # Se actualiza el valor de las frecuencias para calcular la
-        # probabilidad a priori
-        for clase in self.clases:
-            #  ---------------------------------------------------
-            #  agregar aqui el código
-            self.frec['clases'][clase] += clases.count(clase)
-            #  raise NotImplementedError("Falta cmletar esto para la tarea")
-            #  ---------------------------------------------------
+        # Aqui comienza el código para hacer el llenado de las CPT
 
-            # Ahora se actualiza el valor de las frecuencias por cada atributo y
-            # para cada posible clase        #
-            for (i, var) in enumerate(self.var_nom):
+        # Primero se llena el valor de las cuentas para calcular la probabilidad a priori
+        for clase in self.cls_nombre:
 
-                dato_var_clase = [datos[j][i] for j in range(len(datos))
-                                  if clases[j] == clase]
+            #---------------------------------------------------
+            # agregar aqui el código
+            size = len(clases)
+            for i in range(size):
+                if clases[i] == clase:
+                    self.cuentas['clases'][clase] +=1 
+            #---------------------------------------------------
 
-                for val in self.vals[var]:
-                    #  --------------------------------------------------
-                    #  agregar aquí el código
-                    self.frec[var][clase][val] += dato_var_clase.count(val)
-                    #  raise NotImplementedError("Falta cmletar esto para la tarea")
-                    #  --------------------------------------------------
+        # Ahora se llena el valor de las cuentas por cada atributo y para cada posible clase
+        for i in range(len(self.vars)):
+            for clase in self.cls_nombre:
 
-        # Ahora hay que actualizar al final los logaritmos de las
-        # probabilidades para hacer el reconocimiento muy rápido (Usar
-        # únicamente la información de self.frec par hacer esto)
-        N = sum([self.frec['clases'][cls] for cls in clases])
-        for clase in clases:
-            #  ---------------------------------------------------
-            #  agregar aqui el código
-            Nc = self.frec['clases'][clase]
-            self.log_probs['clases'][clase] = log(N/Nc)
-            #  raise NotImplementedError("Falta cmletar esto para la tarea")
-            #  ---------------------------------------------------
-
-            # Ahora se actualiza la probabilidad por cada atributo y
-            # para cada posible clase        #
-            for var in self.var_nom:
-                for val in self.vals[var]:
-                    #  --------------------------------------------------
-                    #  agregar aquí el código
-                    Ncv = self.frec[var][clase][val]
-                    K = len(self.vals[var])
-                    self.log_probs[var][clase][val] = log((Ncv + 1)/(Nc + K))
-                    #  raise NotImplementedError("Falta cmletar esto para la tarea")
-                    #  --------------------------------------------------
+                #--------------------------------------------------
+                # agregar aquí el código
+                for j in self.cuentas[self.vars[i]][clase]:
+                    for k in range(len(datos)):
+                        if datos[k][i] == j and clases[k] == clase:
+                            self.cuentas[self.vars[i]][clase][j] += 1
+                #--------------------------------------------------
 
     def reconoce(self, datos):
         """
-        Identifica la clase a la que pertenece cada uno de los datos que
-        se solicite, de acuerdo al clasificador.
-
-        @param datos = [dato_1, dato_2, ...] es una lista de datos
-                       para clasificar, donde dato_i = [dato_i,1,
-                       ..., dato_i,n] es el valor del dato en cada
-                       atributo. Hay que recordar que se puede
-                       utilizar el método de naive bayes si no se
-                       conocen todos los atributos. Si un atributo no
-                       se conoce dato(i,n) = None.
-
+        Identifica la clase a la que pertenece cada uno de los datos que se solicite, de acuerdo al clasificador
+        datos = [dato(1), dato(2), ...] es una lista de datos para clasificar, donde
+        dato(i) = [dato(i,1), ..., dato(i,n)] es el valor del dato en cada atributo. Hay que recordar que
+        se puede utilizar el método de naive bayes si no se conocen todos los atributos. Si un atributo no se conoce,
+        entonces dato(i,k) == None.
+        Para esto vamos a utilizar el clasificador de Naive Bayes con el ajuste Laplaciano.
         """
         clases = []
 
-        #  ---------------------------------------------------
-        #  agregar aquí el código
+        #---------------------------------------------------
+        # agregar aquí el código
+        #---------------------------------------------------
 
-        def log_prob(dato, clase):
-            return (self.log_probs['clases'][clase] +
-                    sum([self.log_probs[var][clase][dato[i]]
-                         for (i, var) in enumerate(self.var_nom)]))
+        for clase in self.cls_nombre:
+            self.num_datos += self.cuentas['clases'][clase]
+            for i in range(len(datos)):
+                inicial = -1.0
+                for c in self.cls_nombre:
+                    nuevo = 1.0
 
-        clases = [max(self.clases, key=lambda clase: log_prob(dato, clase))
-                  for dato in datos]
-        #  ---------------------------------------------------
+                    nuevo *= self.cuentas['clases'][c]/self.num_datos
+
+                    #mejor = None <-- ESTA COSA FEA 
+
+                    if nuevo > inicial:
+                        mejor = clase
+                        inicial = nuevo
+
+            clases.append(mejor)    #Si se pone en el ciclo más externo, los asserts funcionan... Si se pone aqui no funcionan pero spam_filter si funciona.. help
+
         return clases
 
 
 def test():
     """
     Esta funcion sirve para poder ir probando y corrigiendo el programa.
-
-    Hay 3 pruebas básicas, una para probar la inicialización, otra
-    para probar el aprendizaje (o el llenado de cuentas) y la 3ra para
-    probar si el reconocimiento se hace correctamente. hasta que pasen
-    todas las pruebas no hay que pasar al problema que se encuentra en
-    el archivo naive_bayes.py
-
+    Hay 3 pruebas básicas, una para probar la inicialización, otra para probar el aprendizaje (o el llenado de cuentas)
+    y la 3ra para probar si el reconocimiento se hace correctamente. hasta que pasen todas las pruebas no hay que pasar
+    al problema que se encuentra en el archivo naive_bayes.py
     """
 
-    clases = {'N', 'P'}
-    variables = ['uno', 'dos']
-    valores = {'uno': {1, 2, 3, 4}, 'dos': {10, 20}}
-    nb = NaiveBayes(clases, variables, valores)
+    variables = ('uno', 'dos')
+    valores = dict(uno={1, 2, 3, 4}, dos={10, 20})
+    id_clases = {'N', 'P'}
+    nb = NaiveBayes(variables, valores, id_clases)
 
-    assert nb.frec['clases'] == {'N': 0, 'P': 0}
-    assert nb.frec['uno']['N'] == {1: 0, 2: 0, 3: 0, 4: 0}
-    print("La primera prueba se completo con exito")
+    assert nb.cuentas['clases'] == {'N': 0, 'P': 0}
+    assert nb.cuentas['uno']['N'] == {1: 0, 2: 0, 3: 0, 4: 0}
 
-    data = [[1, 10], [2, 10], [3, 10], [4, 10],
-            [1, 20], [2, 20], [3, 20], [4, 20]]
-    clases = ['N', 'P', 'P', 'N', 'N', 'P', 'N', 'N']
+    print "La primera prueba se completo con exito"
 
-    nb = NaiveBayes()
-    assert nb.frec is None
+    data = [[1, 10], [2, 10], [3, 10], [4, 10], [1, 20], [2, 20], [3, 20], [4, 20]]
+    clases = ['N',     'P',    'P',      'N',      'N',     'P',    'N',     'N']
+
+    nb = NaiveBayes(('uno', 'dos'))
+
+    assert nb.cuentas is None
 
     nb.aprende(data, clases)
-    assert nb.frec['clases'] == {'N': 5, 'P': 3}
-    assert nb.frec['0']['P'] == {1: 0, 2: 2, 3: 1, 4: 0}
-    assert nb.frec['0']['N'] == {1: 2, 2: 0, 3: 1, 4: 2}
-    assert nb.frec['1']['P'] == {10: 2, 20: 1}
-    assert nb.frec['1']['N'] == {10: 2, 20: 3}
-    print("La segunda prueba se completó con exito")
+    assert nb.cuentas['clases'] == {'N': 5, 'P': 3}
+    assert nb.cuentas['uno']['N'] == {1: 2, 2: 0, 3: 1, 4: 2}
+    assert nb.cuentas['dos']['P'] == {10: 2, 20: 1}
+    assert nb.cuentas['dos']['N'] == {10: 2, 20: 3}
 
-    assert nb.log_probs['clases']['N'] == log(5/8)
-    assert nb.frec['0']['P'][1] == log(1/7)
-    assert nb.frec['1']['N'][20] == log(4/7)
-    print("La tercera prueba se completó con exito")
+    print "La segunda prueba se completó con exito"
 
     data_test = [[2, 20], [4, 10]]
     clase_test = nb.reconoce(data_test)
-    print(clase_test)
     assert clase_test == ['P', 'N']
-    print("La cuarta prueba se completó con exito")
 
+    print "La tercera prueba se completó con exito"
 
 if __name__ == "__main__":
     test()
